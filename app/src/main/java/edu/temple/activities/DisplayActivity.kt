a4b9e1d024bc99c0e98e231aa1dfce81d23435fd
Timothy <timkhumpan@gmail.com>
2024-10-03 14:51:38 -0400

    create launcher and button onclick event

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,14 +1,20 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-
+import androidx.activity.result.contract.ActivityResultContracts
+const val message = "message"
 class DisplayActivity : AppCompatActivity() {
 
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == RESULT_OK){
 
+        }
+    }
     // TODO Step 3: Use returned value for lyricsDisplayTextView text size
 
     private lateinit var lyricsDisplayTextView: TextView
@@ -20,6 +26,11 @@ class DisplayActivity : AppCompatActivity() {
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
+
+        textSizeSelectorButton.setOnClickListener{
+            val launchIntent = Intent(this@DisplayActivity, TextSizeActivity::class.java)
+            launcher.launch(launchIntent)
+        }
 
     }
 }
